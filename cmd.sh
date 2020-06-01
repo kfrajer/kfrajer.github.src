0c512c9fd1e71a4f7699a381001b32324a6bee5b
kfrajer <krisfrajer@gmail.com>
2020-06-01 18:17:18 -0400

    Pushing arguments from cmd.sh to inner script

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-
 chmod +x crlf-cleanup.sh gh-deploy.sh
 dos2unix gh-deploy.sh
-./gh-deploy.sh 
+
+# $@ works for multiple arguments
+# Always put it in double-quotes to avoid misparsing of arguments containing spaces or wildcards
+# Note: $0 is not in $@
+# Note: "$*" will be passed as one long string
+./gh-deploy.sh "$@"
